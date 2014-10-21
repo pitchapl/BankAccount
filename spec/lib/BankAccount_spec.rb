c52475a152f4edb1402addb02e46066f7504b7d6
pitchapl <pitcha.pl@hotmail.com>
2014-10-21 20:09:51 +0700

    example3

--- spec/lib/BankAccount_spec.rb
+++ spec/lib/BankAccount_spec.rb
@@ -13,5 +13,14 @@ describe BankAccount do
       balance=account1.withdraw(500)
       expect(balance).to eq(4500)
     end
+     it "check the transaction" do
+   	  account1=BankAccount.new(5000)
+      account1.withdraw(300)
+      account1.deposit(60000)
+      account1.deposit(60000)
+      account1.withdraw(50000)
+      count=account1.numberOfTransactions()
+      expect(count).to eq(4)
+    end
 end
 end
