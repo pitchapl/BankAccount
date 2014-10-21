42ec20c9796fd463b54811c2adcb02dd459a3000
pitchapl <pitcha.pl@hotmail.com>
2014-10-21 20:08:49 +0700

    example2

--- spec/lib/BankAccount_spec.rb
+++ spec/lib/BankAccount_spec.rb
@@ -8,5 +8,10 @@ describe BankAccount do
       balance=account1.deposit(40000)
       expect(balance).to eq(45000)
     end
+    it "check the withdraw" do
+   	  account1=BankAccount.new(5000)
+      balance=account1.withdraw(500)
+      expect(balance).to eq(4500)
+    end
 end
 end
